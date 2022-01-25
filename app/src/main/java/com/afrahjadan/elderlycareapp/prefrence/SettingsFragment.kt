5ae4f7e99ca29119d832f20d6033e16d523f707b
Afrah <Afrah.suliman.ruh.k@tuwaiq.edu.sa>
2022-01-25 22:43:05 +0300

    delete

---
+++ app/src/main/java/com/afrahjadan/elderlycareapp/prefrence/SettingsFragment.kt
@@ -0,0 +1,67 @@
+package com.afrahjadan.elderlycareapp.prefrence
+
+import android.content.SharedPreferences
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.preference.ListPreference
+import androidx.preference.Preference
+import androidx.preference.PreferenceFragmentCompat
+import com.afrahjadan.elderlycareapp.R
+import java.util.*
+
+class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
+
+    private val themeProvider by lazy { ThemeProvider(requireContext()) }
+    private val themePreference by lazy {
+        findPreference<ListPreference>(getString(R.string.theme_preferences_key))
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.settings, rootKey)
+        setThemePreference()
+    }
+
+    private fun setThemePreference() {
+        themePreference?.onPreferenceChangeListener =
+            Preference.OnPreferenceChangeListener { _, newValue ->
+                if (newValue is String) {
+                    val theme = themeProvider.getTheme(newValue)
+                    AppCompatDelegate.setDefaultNightMode(theme)
+                }
+                true
+            }
+
+        themePreference?.summaryProvider = getThemeSummaryProvider()
+    }
+
+    private fun getThemeSummaryProvider() =
+        Preference.SummaryProvider<ListPreference> { preference ->
+            themeProvider.getThemeDescriptionForPreference(preference.value)
+        }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        if (key == "language") {
+            val prefs = sharedPreferences?.getString(key, "1")
+
+            when (prefs?.toInt()) {
+                1 -> {
+                    val locale = Locale("en")
+                    Locale.setDefault(locale)
+                    val config = Configuration()
+                    config.setLocale(locale)
+                    context?.getResources()?.updateConfiguration(config, null)
+                }
+                2 -> {
+
+                    val locale = Locale("ar")
+                    Locale.setDefault(locale)
+                    val config = Configuration()
+                    config.setLocale(locale)
+                    context?.resources?.updateConfiguration(config, null)
+
+                }
+            }
+        }
+    }
+}
